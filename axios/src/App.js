b587fc1ecbe7636a58c54376be9574f9ab9e0838
lizhiqiang17 <lizhiqiang17@58.com>
2022-06-20 22:12:59 +0800

    axios的基本使用

--- axios/src/App.js
+++ axios/src/App.js
@@ -1,10 +1,45 @@
 import React, {Component} from 'react';
+import axios from "axios";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+        // 1、axios发送基本网络请求
+        axios({
+            url: 'http://httpbin.org/get',
+            params: {
+                name: 'why',
+                age: 18
+            }
+        }).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        });
+
+
+        axios({
+            url: 'http://httpbin.org/post',
+            data: {
+                name: 'why',
+                age: 18
+            },
+            method: 'post'
+        }).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
     render() {
         return (
             <div>
-              Component1
+                Component1
             </div>
         );
     }
