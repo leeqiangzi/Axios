ff35eeae6349f33617212c0d1538f0a2f39dc233
lizhiqiang17 <lizhiqiang17@58.com>
2022-06-22 15:25:39 +0800

    axios默认配置

--- axios/src/index.js
+++ axios/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
+import axios from "axios";
+
+// axios默认配置
+// axios.defaults.baseURL = 'https://xxxx';
+// axios.defaults.timeout = 5000;
+// ......
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
