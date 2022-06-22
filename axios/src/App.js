4881cad29f1fa3f84c85f0eed59afed5009f30d3
lizhiqiang17 <lizhiqiang17@58.com>
2022-06-22 14:37:50 +0800

    合并多个网络请求：axios.all

--- axios/src/App.js
+++ axios/src/App.js
@@ -8,41 +8,78 @@ class App extends Component {
     }
 
     componentDidMount() {
-        // // 1、axios发送基本网络请求
-        // axios({
-        //     url: 'http://httpbin.org/get',
-        //     params: {
-        //         name: 'why',
-        //         age: 18
-        //     }
-        // }).then(res => {
-        //     console.log(res);
-        // }).catch(error => {
-        //     console.log(error);
-        // });
-        //
-        //
-        // axios({
-        //     url: 'http://httpbin.org/post',
-        //     data: {
-        //         name: 'why',
-        //         age: 18
-        //     },
-        //     method: 'post'
-        // }).then(res => {
-        //     console.log(res);
-        // }).catch(error => {
-        //     console.log(error);
-        // });
 
-        // async await
-        this._fetchData();
+        this._fetchAll();
     }
 
+    /**
+     * 合并多个网络请求
+     * @private
+     */
+    _fetchAll() {
+        const request1 = axios({
+            url: 'http://httpbin.org/get',
+            params: {name: 'why', age: 18}
+        });
+        const request2 = axios({
+            url: 'http://httpbin.org/post',
+            data: {name: 'why', age: 18},
+            method: 'post'
+        });
+
+        axios.all([request1, request2]).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    /**
+     * post请求
+     * @private
+     */
+    _fetchPost() {
+        axios({
+            url: 'http://httpbin.org/post',
+            data: {
+                name: 'why',
+                age: 18
+            },
+            method: 'post'
+        }).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    /**
+     * get请求
+     * @private
+     */
+    _fetchGet() {
+        axios({
+            url: 'http://httpbin.org/get',
+            params: {
+                name: 'why',
+                age: 18
+            }
+        }).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    /**
+     * async await
+     * @returns {Promise<void>}
+     * @private
+     */
     async _fetchData() {
         try {
             const result = await axios.get("http://httpbin.org/get", {
-                params:{
+                params: {
                     name: 'why',
                     age: 18
                 }
