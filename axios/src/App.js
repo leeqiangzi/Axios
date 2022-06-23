a6a54c9104fcd7fa79b1031248078546e4730a0c
lizhiqiang17 <lizhiqiang17@58.com>
2022-06-23 20:05:36 +0800

    axios拦截器

--- axios/src/App.js
+++ axios/src/App.js
@@ -9,7 +9,55 @@ class App extends Component {
 
     componentDidMount() {
 
-        this._fetchAll();
+        this._fetchInterceptor();
+    }
+
+    // 拦截器
+    _fetchInterceptor() {
+        // 请求拦截
+        axios.interceptors.request.use(config => {
+            /**
+             * 1、发送网络请求时，在页面的中间位置显示loading的组件
+             *
+             * 2、某一些请求要求用户必须携带token，如果没有携带，那么直接跳转到登录页面
+             *
+             * 3、params/data序列化操作
+             */
+            return config;
+        }, error => {
+            // 一般不会在这里拦截
+        });
+
+        // 响应拦截
+        axios.interceptors.response.use(res => {
+            return res.data;
+        }, error => {
+            if (error && error.response) {
+                switch (error.response.status) {
+                    case 400:
+                        console.log('请求错误');
+                        break;
+                    case 401:
+                        console.log('未授权错误');
+                        break;
+                    default:
+                        console.log('其他错误');
+                        break;
+                }
+            }
+            return error;
+        });
+
+
+        axios.get('http://httpbin.org/get', {
+            params: {
+                name: 'why'
+            }
+        }).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        })
     }
 
     /**
